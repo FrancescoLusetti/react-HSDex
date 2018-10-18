887aecc05cb9216090d933a0d0feffbbb708fe62
Francesco Lusetti <francesco.lusetti13@gmail.com>
2018-10-18 10:03:36 +0200

    Format changing

--- src/HSCard.js
+++ src/HSCard.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardTitle, CardText,CardSubtitle, CardBody } from 'reactstrap';
 
 class HSCard extends Component {
+    constructor(props){
+        super(props);
+        card=this.props.card
+    }
     render(){
         let card=this.props.card;
         if(card.type=="MINION"){
@@ -9,7 +13,7 @@ class HSCard extends Component {
                 <CardImg top width="100%" src={"https://art.hearthstonejson.com/v1/render/latest/enUS/256x/"+card.id+".png"} alt={card.name} />
                 <CardBody>
                   <CardTitle>Name: {card.name}</CardTitle>
-                  <CardSubtitle>Type: {card.type} - Cost: {card.cost} - Rarity:{card.rarity}</CardSubtitle>
+                  <CardSubtitle>Type: {card.type} - Cost: {card.cost} - Rarity: {card.rarity}</CardSubtitle>
                   <CardText>Effect: {card.text}<br/>Attack: {card.attack} - Life: {card.attack}</CardText>
                 </CardBody>
               </Card>)
@@ -19,8 +23,8 @@ class HSCard extends Component {
                 <CardImg top width="100%" src={"https://art.hearthstonejson.com/v1/render/latest/enUS/256x/"+card.id+".png"} alt={card.name} />
                 <CardBody>
                   <CardTitle>Name: {card.name}</CardTitle>
-                  <CardSubtitle>Type: {card.type} - Cost: {card.cost} - Rarity:{card.rarity}</CardSubtitle>
-                  <CardText>Effect: {card.text}</CardText>
+                  <CardSubtitle>Type: {card.type} - Cost: {card.cost} - Rarity: {card.rarity}</CardSubtitle>
+                  <CardText>Effect: {card.text}/></CardText>
                 </CardBody>
               </Card>)
         }  
